3685d9b512514b78f6ca2e24bb4cab14d9c28e86
yjc <424586730@qq.com>
2021-03-16 08:13:24 +0800

    添加任务栏图标,退出对话框

---
+++ Qt-Zhku-Client/CloseDialog.cpp
@@ -0,0 +1,14 @@
+#include "CloseDialog.h"
+#include "ui_CloseDialog.h"
+
+CloseDialog::CloseDialog(QWidget *parent) :
+    QDialog(parent),
+    ui(new Ui::CloseDialog)
+{
+    ui->setupUi(this);
+}
+
+CloseDialog::~CloseDialog()
+{
+    delete ui;
+}
