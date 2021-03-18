be4fa02e0d3f648139466452742498be4bcee7bc
424586730@qq.com <424586730@qq.com>
2021-03-18 11:10:57 +0800

    差一点做好等级考试查询

---
+++ Qt-Zhku-Client/Rankexam_Ui.cpp
@@ -0,0 +1,14 @@
+#include "Rankexam_Ui.h"
+#include "ui_RankExam_Ui.h"
+
+RankExam_Ui::RankExam_Ui(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::RankExam_Ui)
+{
+    ui->setupUi(this);
+}
+
+RankExam_Ui::~RankExam_Ui()
+{
+    delete ui;
+}
---
+++ Qt-Zhku-Client/Rankexam_Ui.h
@@ -0,0 +1,22 @@
+#ifndef RANKEXAM_UI_H
+#define RANKEXAM_UI_H
+
+#include <QWidget>
+
+namespace Ui {
+class RankExam_Ui;
+}
+
+class RankExam_Ui : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit RankExam_Ui(QWidget *parent = 0);
+    ~RankExam_Ui();
+
+private:
+    Ui::RankExam_Ui *ui;
+};
+
+#endif // RANKEXAM_UI_H
